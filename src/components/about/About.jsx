ad91a861fbd83798ddc3fcdc6ee4cddb9a8a7629
kingorianderson <kingorianderson@gmail.com>
2022-02-24 22:01:05 +0300

    commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './about.css'
+import About from '../../assets/about.jpg'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            <img src={About} alt='About Image'/>
+          </div>
+        </div>
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,15 +3,17 @@ import './nav.css'
 import { BiBook, BiHomeAlt, BiMessageSquareDetail } from 'react-icons/bi'
 import { AiOutlineUser } from 'react-icons/ai'
 import { RiServiceLine } from 'react-icons/ri'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><BiHomeAlt/></a>
-      <a href="#about"><AiOutlineUser/> </a>
-      <a href="#skills"><BiBook/> </a>
-      <a href="#services"><RiServiceLine/> </a>
-      <a href="#contact"><BiMessageSquareDetail/> </a>
+      <a href="#" onClick={() => setActiveNav('#')} className= {activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className= {activeNav === '#about' ? 'active' : ''}><AiOutlineUser/> </a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className= {activeNav === '#skills' ? 'active' : ''}><BiBook/> </a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className= {activeNav === '#services' ? 'active' : ''}><RiServiceLine/> </a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className= {activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/> </a>
     </nav>
   )
 }
