30c3730e46921f641c3ef7b426e73b29543cd701
kingorianderson <kingorianderson@gmail.com>
2022-02-25 11:11:03 +0300

    commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
-import About from '../../assets/about.jpg'
+import Img from '../../assets/about.jpg'
+import { FaAward, FaUserFriends } from 'react-icons/fa'
+import { BsFolderFill} from 'react-icons/bs'
+
 
 const About = () => {
   return (
@@ -11,14 +14,25 @@ const About = () => {
       <div className='container about_container'>
         <div className='about_me'>
           <div className='about_me-image'>
-            <img src={About} alt='About Image'/>
+            <img src={Img} alt='About Image'/>
           </div>
         </div>
         <div className='about_content'>
           <div className='about_cards'>
             <article className='about_card'>
+              <FaAward about='about_icon'/>
               <h5>Experience</h5>
               <small>3+ Years Working</small>
+            </article>
+            <article className='about_card'>
+              <FaUserFriends about='about_icon'/>
+              <h5>Clients</h5>
+              <small>60+ Worldwide</small>
+            </article>
+            <article className='about_card'>
+              <BsFolderFill about='about_icon'/>
+              <h5>Projects</h5>
+              <small>100+ Completed</small>
             </article>
           </div>
         </div>
