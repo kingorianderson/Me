5adba3863c7724f90b33e60f9b6d9d7e5f993dc7
kingorianderson <kingorianderson@gmail.com>
2022-03-03 14:13:50 +0300

    improvement on tabs

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -17,8 +17,7 @@ function Experience() {
   return ( 
     
     <section className="skills section" id="skills">
-        <h2 className="section__title">Skills</h2>
-        <span className="section__subtitle">My Technical Level </span>
+      
 
         <div className="skills__container container grid">
           <div>
--- src/components/tabs/Tabs.js
+++ src/components/tabs/Tabs.js
@@ -13,25 +13,29 @@ function Tabs() {
   return (
     <div className="container">
       <ul className="tab-nav">
-          <li>
+          
         <button
           className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(1)}                          
-        ><h2> Skills</h2>
-         
-        </button> </li>  <li>
+        ><h2> Skills</h2> 
+        </button> 
+
+        <li></li>  
+
         <button
           className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(2)}
-        >
-          <h2>Experience</h2>
-        </button> </li> <li>
+        ><h2>Experience</h2>
+        </button> 
+
+        <li></li> 
+
         <button
           className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
           onClick={() => toggleTab(3)}
-        >
-          <h2>Education</h2>
-        </button> </li>
+        ><h2>Education</h2>
+        </button>
+        
       </ul>
 
 
@@ -90,13 +94,12 @@ function Tabs() {
 
         </div>
 
+
+
         <div
           className={toggleState === 2 ? "content  active-content" : "content"}
         >
          
-      
-
-          
           <div class="qualification__sections">
             
             <div class="qualification__content qualification__active" data-content id="education">
@@ -116,17 +119,13 @@ function Tabs() {
               </div>
               
               <div class="qualification__data">
-                <div></div>
                 <div>
                   <span class="qualification__rounder"></span>
-                
                 </div>
 
                 <div>
                   <h3 class="qualification__title">BSc Computer Technology</h3>
-                  <span class="qualification__subtitle"
-                    >Meru University of Sci. and Technology</span
-                  >
+                  <span class="qualification__subtitle" >Meru University of Sci. and Technology</span>
                   <div class="qualification__calendar">
                     <i class="uil uil-calendar-alt">2016 - 2020</i>
                   </div>
@@ -134,24 +133,18 @@ function Tabs() {
               </div>
               
               
-              <div class="qualification__data">
-                <div></div>
-                <div>
-                  <span class="qualification__rounder"></span>
-                 
-            </div>
-           
-            
-          </div>
         </div>
         </div>
         </div>
+
 
         <div
           className={toggleState === 3 ? "content  active-content" : "content"}
         > 
         <Experience/>
         </div>
+
+
       </div>
     </div>
   );
